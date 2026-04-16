9471a1fed49bf3e8f376ef38579d8c2c166c4825
argenestic <argenestic@gmail.com>
2026-04-16 15:13:52 -0300

    Add files via upload

---
+++ EX1.cpp
@@ -0,0 +1,10 @@
+
+/*exercicio 01*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Bem-vindo ao C++!"<< endl;
+    return 0;
+}
---
+++ EX2.cpp
@@ -0,0 +1,10 @@
+
+/*exercicio 02*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Yam"<< endl;
+    return 0;
+}
